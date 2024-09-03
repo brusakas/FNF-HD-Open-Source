2e28f111b9648ff9abb573975d2aaf6aff2eada2
a-random441 <somerandomgithubacc@gmail.com>
2024-09-03 13:59:51 +0100

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2690,7 +2690,7 @@ class PlayState extends MusicBeatState
 
 				if (SONG.validScore)
 				{
-					NGio.unlockMedal(60961);
+					//NGio.unlockMedal(60961);
 					Highscore.saveWeekScore(storyWeek, campaignScore, storyDifficulty);
 				}
 
