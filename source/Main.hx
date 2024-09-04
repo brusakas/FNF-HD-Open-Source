ee8c7bebfce616536c2b755eb50b51741090f57e
brusakas <121342424+brusakas@users.noreply.github.com>
2024-09-04 20:36:21 +0100

    thing

--- source/Main.hx
+++ source/Main.hx
@@ -7,7 +7,7 @@ import openfl.Lib;
 import openfl.display.FPS;
 import openfl.display.Sprite;
 import openfl.events.Event;
-import lime.system.System;
+//import lime.system.System;
 
 class Main extends Sprite
 {
@@ -15,11 +15,11 @@ class Main extends Sprite
 	var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
 	var initialState:Class<FlxState> = Startup; // The FlxState the game starts with.
 	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
-	var framerate:Int = 144; // How many frames per second the game should run at.
+	var framerate:Int = 60; // How many frames per second the game should run at.
 	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
 	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
 
-	public static var path:String = System.applicationStorageDirectory;
+	//public static var path:String = System.applicationStorageDirectory;
 
 	// You can pretty much ignore everything from here on - your code should go in your states.
 
